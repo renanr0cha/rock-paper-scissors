c85540c4659beb2c84034221ebe5f02f50005841
Renan <renan.aragorn@gmail.com>
2022-04-28 18:02:58 -0300

    refactoring started

--- script.js
+++ script.js
@@ -4,49 +4,49 @@ let computerSelection = "";
 let playerSelection = "";
 let computerScore = 0;
 let playerScore = 0;
-let playerName = "";
 let playerExampleNames = ['Gandalf', 'Harry Potter', 'Neo', 'John Connor', 'Luke Skywalker', 'Naruto', 'Ragnar Lothbrok', 'Jon Snow', 'Jack Bauer'];
 
+let computerScoreboard = document.querySelector('#computer-score-number');
+let playerScoreboard = document.querySelector('#person-score-number');
+computerScoreboard.textContent = computerScore;
+playerScoreboard.textContent = playerScore;
+let personName = document.querySelector("#person-name");
+let attributeChoice = document.querySelector(".choice");
+let divNameInput = document.querySelector("#choose-name-div");
+let nameInput = document.querySelector("#input-name");
+let buttons = document.querySelector("#buttons");
+let intro = document.querySelector("#intro-text");
+let audio = document.querySelector("#battle-song");
+let startButton = document.querySelector("#start-match")
+let rockButton = document.querySelector("#rock")
+let paperButton = document.querySelector("#paper")
+let scissorsButton = document.querySelector("#scissors")
 
-var confettiSettings = { target: 'my-canvas' };
-var confetti = new ConfettiGenerator(confettiSettings);
-
-let computerScoreboard = document.getElementById('computer-score-number');
-let playerScoreboard = document.getElementById('person-score-number');
-computerScoreboard.innerHTML = computerScore;
-playerScoreboard.innerHTML = playerScore;
-let messageAfterRound = document.getElementById("message");
-let personName = document.getElementById("person-name");
-let attributeChoice = document.getElementsByClassName("choice");
-let hideNameInput = document.getElementById("choose-name-div");
-let nameInput = document.getElementById("input-name");
-let buttons = document.getElementById("buttons");
-let intro = document.getElementById("intro-text");
-let audio = document.getElementById("battle-song");
 
 function randomName () {
   let random = Math.floor(Math.random() * playerExampleNames.length);
-  playerName = playerExampleNames[random].toUpperCase();
-  personName.innerHTML = playerName;
-  hideNameInput.removeChild(nameInput);
-  hideNameInput.removeChild(buttons);
-  playSound();
-  introText();
+  personName.textContent = playerExampleNames[random].toUpperCase();
+  introText()
 }
 
-function nameIsSet () {
-  if (playerName != "") {
+function beginMatch() {
+  rockButton.classList.remove("hide")
+  paperButton.classList.remove("hide")
+  scissorsButton.classList.remove("hide")
+  document.querySelector("#choose-weapon").classList.remove("hide")
+  startButton.classList.add("hide")
+  intro.textContent = ""
 
-  }
 }
 
 function setName () {
-  playerName = nameInput.value.toUpperCase();
-  personName.innerHTML = playerName;
-  hideNameInput.removeChild(nameInput);
-  hideNameInput.removeChild(buttons);
-  playSound();
-  introText();
+  personName.textContent = nameInput.value.toUpperCase();
+
+  if (personName.textContent == "") {
+    return alert("You have to choose a name first!")
+  } else {
+    introText()
+  }
 }
 
 function playSound() {
@@ -55,73 +55,57 @@ function playSound() {
 
 
 function introText () {
-  intro.innerHTML = `Hello ${playerName}, the world is on the verge of collapsing to the Machines, that dominated everything. You are our last hope!`
-  
+  nameInput.classList.add("hide")
+  buttons.children[0].classList.add("hide")
+  buttons.children[1].classList.add("hide")
+  startButton.classList.remove("hide")
+
+  playSound();
+  intro.textContent = 
+  `Hello ${personName.textContent}, the world is on the verge of collapsing to the Machines.
+  They dominated everything. 
+  You are our last hope!`
 }
 
 
 function computerPlay() {
   let random = Math.floor(Math.random() * 3);
-  if (random == 1) {
-      return "rock";
-  } else if (random == 2) {
-      return "paper";
-  } else {
-      return "scissors";
+  switch(random) {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    case 2:
+      return "scissors"
+    default:
+
   }
 }
 
-function playerChooseRock() {
-  if (playerName != "") {
-    playerSelection = 'rock';
-  computerSelection = computerPlay();
-  playRound(playerSelection, computerSelection);
-  isOver();
-  } else {
-    alert("You have to choose a name first!")
-  }
+function playerPlay(choice) {
+  playerSelection = choice.id
+  computerSelection = computerPlay()
+  playRound(playerSelection, computerSelection)
+  isOver()
 }
 
-
-
-function playerChoosePaper() {
-  if (playerName != "") {
-    playerSelection = 'paper';
-    computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    isOver();
-  } else {
-    alert("You have to choose a name first!")
-  }
-}
-
-function playerChooseScissors() {
-  if (playerName != "") {
-    playerSelection = 'scissors';
-    computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    isOver();
-  } else {
-    alert("You have to choose a name first!")
-  }
-}
 
 function lose() {
   computerScore++;
-  computerScoreboard.innerHTML = computerScore;
-  messageAfterRound.innerHTML = "Oh no, you lose the round! The Machines are closer to wipe us from the face of the Earth!"
+  computerScoreboard.textContent = computerScore;
+  intro.textContent = "Oh no, you lose the round! The Machines are closer to wipe us from the face of the Earth!"
   
 }
 
 function win() {
   playerScore++;
-  playerScoreboard.innerHTML = playerScore;
-  messageAfterRound.innerHTML = "Yes baby, you win the round! We are closer to send them to the junkyard! Keep up!"
+  playerScoreboard.textContent = playerScore;
+  intro.textContent = "Yes baby, you win the round! We are closer to send them to the junkyard! Keep up!"
 }
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock") {
     if (computerSelection == "rock") {
-      return messageAfterRound.innerHTML = "It's a draw! The battle is toe to toe! God help us!";
+      return intro.textContent = "It's a draw! The battle is toe to toe! God help us!";
     } else if (computerSelection == "paper"){
       return lose();
     } else { 
@@ -131,7 +115,7 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection == "rock") {
       return win();
     } else if (computerSelection == "paper") {
-      return messageAfterRound.innerHTML = "It's a draw! The battle is toe to toe! God help us!";
+      return intro.textContent = "It's a draw! The battle is toe to toe! God help us!";
     } else {
       return lose();
     }
@@ -141,22 +125,20 @@ function playRound(playerSelection, computerSelection) {
     } else if (computerSelection == "paper") {
       return win();
     } else {
-      return messageAfterRound.innerHTML = "It's a draw! The battle is toe to toe! God help us!";
+      return intro.textContent = "It's a draw! The battle is toe to toe! God help us!";
     }
   }
 }
 
 function isOver() {
-  console.log(playerScore + ' X ' + computerScore)
   if (playerScore >= 5 || computerScore >= 5) {
-    attributeChoice[0].removeAttribute('onclick');
-    attributeChoice[1].removeAttribute('onclick');
-    attributeChoice[2].removeAttribute('onclick');
+    rockButton.removeAttribute('onclick');
+    paperButton.removeAttribute('onclick');
+    scissorsButton.removeAttribute('onclick');
     if (playerScore > computerScore) {
-      messageAfterRound.innerHTML = "WE WON! Congratulations, now we have to repopulate the Earth as fast as possible, humanity triumphs!";
-      confetti.render();
+      intro.textContent = "WE WON! Congratulations, now we have to repopulate the Earth as fast as possible, humanity triumphs!";
     } else {
-      messageAfterRound.innerHTML = "WE LOST! Too bad, you are really bad at this game, now we are extinct because of your incompetence!";
+      intro.textContent = "WE LOST! Too bad, you are really bad at this game, now we are extinct because of your incompetence!";
     }
   } else {
 
